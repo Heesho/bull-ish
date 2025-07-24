358a3f22815644eaa229e18d374830f81be62890
Heesho <hisham11027@gmail.com>
2025-07-24 16:07:34 -0400

    removed unused constants message size and queue size

--- contracts/Wheel.sol
+++ contracts/Wheel.sol
@@ -47,8 +47,6 @@ contract VaultToken is ERC20, Ownable {
 contract Wheel is IEntropyConsumer, ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
 
-    uint256 public constant QUEUE_SIZE = 300;
-    uint256 public constant MESSAGE_LENGTH = 69;
     uint256 public constant MAX_FEE_SPLIT = 50;
     uint256 public constant MIN_FEE_SPLIT = 4;
     uint256 public constant DIVISOR = 100;
